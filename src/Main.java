05498a176bb6e094800b7624154363422f331a3e
Анастасия Сергеевна <kuznecova_18@bk.ru>
2023-07-28 20:01:23 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        int startscore = 100;
+        int replenishment = 1100;
+        int balance;
+
+        if (replenishment <= 1000) {
+            balance = 0;
+        } else {
+            balance = replenishment / 100;
+        }
+        System.out.printf("Бонусы = " + balance);
+
+    }
+}
